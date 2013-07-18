e00b28ef9bbac63d5c85b2b905d6c4e9afe5af01
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2013-07-18 23:24:04 +0800

    7.18 11:22
    加上了注册功能，改了一些登陆和注册的UI。
    整合了toga和黄甲辰的滚动条功能。

--- Kevin/static/script/status.js
+++ Kevin/static/script/status.js
@@ -1,27 +1,112 @@
-var speed;
 $(document).ready(function(){
-    $("#sensorf").mouseenter(function(){
-        $("#sensorf").stop().animate({height:"260px"},500);
-                             });
-    $("#sensorf").mouseleave(function(){
-        $("#sensorf").stop().animate({height:"60px"},500);
-                            });
-    $("#sensorg").mouseenter(function(){
-        $("#sensorg").stop().animate({height:"260px"},500);
-                            });
-    $("#sensorg").mouseleave(function(){
-        $("#sensorg").stop().animate({height:"60px"},500);
-                            });
-    $("#blockf").mouseover(function(e){
-        speed = e.clientY-249;
-        $("#status").html(speed);
-        function a(){
-            $("#slidef").style.top = $("#slidef").style.top - (50-speed)/5;
-            setTimeout(a, 100);
-                           }
-/*function a(e){
-        $("#status").html(e.clientX + " " + (e.clientY- 249));
-                           setTimeout(a,100);*/
-                           });
-                  
-})
+                  cur_i = $("#slidef").height() / 30;
+                  anchor = 0;
+                  cur_y = 0;
+                  cur_b = 0;
+                  active = 0;
+                  $("#sensorf").mouseenter(function(){
+                                           var ht = 272;
+                                           if (Number($("#slidef").height()) + 60 < 272) ht = Number($("#slidef").height()) + 60;
+                                           var time = 2.5 * (ht - 60);
+                                           $("#sensorf").stop().animate({ height: ht.toString()+"px" }, time);
+                                           });
+                  $("#sensorf").mouseleave(function () {
+                                           $("#sensorf").stop().animate({ height: "60px" }, 500);
+                                           });
+                  $("#sensorg").mouseenter(function () {
+                                           var ht = 272;
+                                           if (Number($("#slideg").height() + 60) < 272) ht = Number($("#slideg").height()) + 60;
+                                           var time = 2.5 * (ht - 60);
+                                           $("#sensorg").stop().animate({ height: ht.toString() + "px" }, time);
+                                           });
+                  $("#sensorg").mouseleave(function () {
+                                           $("#sensorg").stop().animate({ height: "60px" }, 500);
+                                           });
+                  /*$("#blockf").mouseover(function (e) {
+                                         speed = e.clientY - 249;
+                                         $("#status").html(speed);
+                                         function a(){
+                                         $("#slidef").style.top = $("#slidef").style.top - (50 - speed) / 5;
+                                         setTimeout(a, 100);
+                                         }
+                                         });*/
+                  function a(){
+                  if(active == 1){
+                  cur_i = $("#slidef").height() / 30;
+                  $("#naruhodo").html(cur_y + "cur_b=" + cur_b + "\n anchor ="+anchor);
+                  /*if(anchor < 0 || anchor >= cur_i)
+                   {
+                   $("#slidef").stop().animate(
+                   top : "0px"
+                   ),
+                   50,
+                   "linear",
+                   function()
+                   {
+                   anchor = 0;
+                   cur_b = 0;
+                   }
+                   }*/
+                  if(cur_b == 0)
+                  {
+                  if((cur_y <= 66) && (anchor != 0))
+                  {
+                  cur_b = 1;
+                  time = (cur_y + 20) * 5;
+                  $("#slidef").stop().animate(
+                                              {
+                                              top : (-(anchor-1)*30).toString() + "px"
+                                              },
+                                              time,
+                                              "linear",
+                                              function()
+                                              {
+                                              anchor --;
+                                              cur_b = 0;
+                                              })
+                  }
+                  if((cur_y>=Number($("#blockf").height())- 66) && (anchor != cur_i - 7))
+                  {
+                  cur_b = 1;
+                  time = (232 - cur_y) * 5;
+                  $("#slidef").stop().animate({
+                                              top : (-(anchor + 1) * 30).toString() + "px"
+                                              },
+                                              time,
+                                              "linear",
+                                              function()
+                                              {
+                                              anchor ++;
+                                              cur_b = 0;
+                                              })
+                  }
+                  }
+                  }
+                  setTimeout(a, 30);
+                  }
+                  a();
+                  $("#blockf").mouseenter(function()
+                                          {
+                                          active = 1;
+                                          })
+                  $("#blockf").mouseover(function(e){
+                                         cur_y = e.clientY - $("#blockf").offset().top;
+                                         })
+                  $("#blockf").mouseleave(function(e){
+                                          active = 0;
+                                          })
+                  $(".fri").mouseenter(function()
+                                       {
+                                       $(this).css("background-color", "#cf3a00");
+                                       $("#blockf").css("overflow", "hidden");
+                                       })
+                  $(".fri").mouseleave(function(){
+                                       $(this).css("background-color", "#df4a10");
+                                       })
+                  $("#blockf").mouseleave(function(){
+                                          $("#slidef").stop().animate({
+                                                                      top : (-anchor * 30).toString() + "px"
+                                                                      },500,"swing",function(){cur_b=0, active=0});
+                                          cur_b=0;
+                                          })
+                  });
--- Kevin/userlogin/views.py
+++ Kevin/userlogin/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User, Permission, Group
 from django.template import RequestContext
 
+usernameList = User.objects.all();
+
 def userlogin(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/home/')
@@ -40,28 +42,66 @@ def home(request):
     else:
         return HttpResponseRedirect('/')
 
-
-############# Quote zhazhazhang ###############
-
 def toRegister(request):
 	c={}
 	c.update(csrf(request))
-	return render_to_response('register.html', c)
+	return render_to_response('register.html', c, context_instance=RequestContext(request))
+
+def checkUsername (a):
+    if len(a) >20:
+        return False
+    for i in range(0,len(a)):
+        p = ord(a[i])
+        if p > 122 or ( p < 97 and p > 90) or (p < 65 and p > 57) or p < 48:
+            if p != 95:
+                return False
+    return True
+
+def checkPassword (a):
+    if len(a) < 6 or len(a) > 30:
+        return False
+    return True
+
+def checkEmail (a):
+    js = 0
+    for i in range(0, len(a)):
+        if a[i] == '@':
+            js+=1
+    if js == 1:
+        return True
+    return False
+
 
 def register(request):
-	#return HttpResponse("page incomplete")
-	newUsername=request.POST.get('newUsername', '')
-	password=request.POST.get('password', '')
-	email=request.POST.get('email', '')
-	if usernameList.filter(username=newUsername):
-		return HttpResponseRedirect('/toRegister/')
-	elif not newUsername or not password:
-		return HttpResponseRedirect('/toRegister/')
-	else:
-		user=User.objects.create_user(username=newUsername, email=email, password=password)
-		user.save()
-		userGroup=UserGroup.objects.create(username=user.username)
-		userGroup.save()
-		return HttpResponseRedirect('/')
+    #return HttpResponse("page incomplete")
+    c={}
+    newUsername=request.POST.get('newUsername', '')
+    password=request.POST.get('password', '')
+    confirmpassword=request.POST.get('confirmpassword', '')
+    email=request.POST.get('email', '')
+    if usernameList.filter(username=newUsername):
+        c['warning'] = "Username already registered!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    elif not newUsername or not password or not confirmpassword:
+        c['warning'] = "Username or password is blank!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    elif password != confirmpassword:
+        c['warning'] = "Confirming pasword is different from password!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    elif not checkUsername (newUsername):
+        c['warning'] = "Invalid username!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    elif not checkPassword (password):
+        c['warning'] = "Invalid password!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    elif not checkEmail (email):
+        c['warning'] = "Invalid email address!"
+        return render_to_response('register.html', c, context_instance=RequestContext(request))
+    else:
+        user=User.objects.create_user(username=newUsername, email=email, password=password)
+        user.save()
+		# userGroup=UserGroup.objects.create(username=user.username)
+		# userGroup.save()
+        return HttpResponseRedirect('/')
 
 ############ QuoteEnd ##########################
